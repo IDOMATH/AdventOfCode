2a9386c65f5dfc78cbe852f7dd3c5ca9c70a3798
IDOMATH <btdothemath@gmail.com>
2024-06-16 15:11:05 -0500

    more setup

--- 2015/Day7/main.go
+++ 2015/Day7/main.go
@@ -4,16 +4,20 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+
+	"github.com/idomath/AdventOfCode/2015/util"
 )
 
 const maxUint16 uint16 = 65535
 
-func main() {
-	// file, err := util.GetFile("./puzzle1_test.txt")
-	// util.CheckErr(err)
-	// lines := util.GetLines(file)
+var outputs map[string]uint16
 
-	// outputs := make(map[string]uint16, len(lines))
+func main() {
+	file, err := util.GetFile("./puzzle1_test.txt")
+	util.CheckErr(err)
+	lines := util.GetLines(file)
+
+	outputs = make(map[string]uint16, len(lines))
 
 	fmt.Println(NotOperator(1))
 
@@ -37,6 +41,18 @@ func ParseInput(input string, outs map[string]uint16) (value uint16) {
 	if len(terms) == 2 {
 		return NotOperator(StrToInt(terms[1]))
 	}
+	if len(terms) == 3 {
+		switch terms[1] {
+		case "AND":
+			return AndOperator(terms[0], terms[2])
+		case "OR":
+			return OrOperator(outs[terms[0]], outs[terms[2]])
+		case "LSHIFT":
+			return outs[terms[0]] << StrToInt(terms[2])
+		case "RSHIFT":
+			return outs[terms[0]] >> StrToInt(terms[2])
+		}
+	}
 
 	return 0
 }
@@ -47,6 +63,15 @@ func StrToInt(input string) uint16 {
 		panic("could not get input number")
 	}
 	return uint16(num)
+}
+
+func AndOperator(x, y string) {
+	var xi uint16
+	i, err := strconv.Atoi(x)
+	if err != nil {
+		xi = outputs[x]
+	}
+	xi = uint16(i)
 }
 
 func NotOperator(num uint16) uint16 {
