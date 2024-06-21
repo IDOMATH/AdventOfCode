9fc4c1aaf01940a52eac4b1e5284637f66ce34d4
IDOMATH <btdothemath@gmail.com>
2024-06-21 16:26:17 -0500

    Cleaning up and adding wire type

--- 2015/Day7/main.go
+++ 2015/Day7/main.go
@@ -10,22 +10,33 @@ import (
 
 const maxUint16 uint16 = 65535
 
-var outputs map[string]uint16
+var outputs map[string]Wire
+
+type Wire struct {
+	Value uint16
+	IsSet bool
+}
 
 func main() {
 	file, err := util.GetFile("./puzzle1_test.txt")
 	util.CheckErr(err)
 	lines := util.GetLines(file)
 
-	outputs = make(map[string]uint16, len(lines))
+	outputs = make(map[string]Wire, len(lines))
 
-	fmt.Println(NotOperator(1))
+	// input, output := SplitInputAndOutput(lines[0])
+	// fmt.Println(ParseInput(input), output)
 
-	// for _, line := range lines {
-	// 	fmt.Println("line: ", line)
-	// 	input, output := SplitInputAndOutput(line)
-	// 	fmt.Println("input: ", input, " output: ", output)
-	// }
+	for _, line := range lines {
+		// fmt.Println("line: ", line)
+		input, output := SplitInputAndOutput(line)
+		// fmt.Println("input: ", ParseInput(input), " output: ", output)
+		outputs[output] = ParseInput(input)
+	}
+
+	for key, output := range outputs {
+		fmt.Println(key, ": ", output)
+	}
 }
 
 func SplitInputAndOutput(instruction string) (input, output string) {
@@ -33,24 +44,24 @@ func SplitInputAndOutput(instruction string) (input, output string) {
 	return puts[0], puts[1]
 }
 
-func ParseInput(input string, outs map[string]uint16) (value uint16) {
+func ParseInput(input string) (value uint16) {
 	terms := strings.Split(input, " ")
-	if len(terms) == 1 {
+	if len(terms) == 2 {
 		return StrToInt(terms[0])
 	}
-	if len(terms) == 2 {
+	if len(terms) == 3 {
 		return NotOperator(StrToInt(terms[1]))
 	}
-	if len(terms) == 3 {
+	if len(terms) == 4 {
 		switch terms[1] {
 		case "AND":
 			return AndOperator(terms[0], terms[2])
 		case "OR":
 			return OrOperator(terms[0], terms[2])
 		case "LSHIFT":
-			return outs[terms[0]] << StrToInt(terms[2])
+			return outputs[terms[0]] << StrToInt(terms[2])
 		case "RSHIFT":
-			return outs[terms[0]] >> StrToInt(terms[2])
+			return outputs[terms[0]] >> StrToInt(terms[2])
 		}
 	}
 
@@ -60,7 +71,7 @@ func ParseInput(input string, outs map[string]uint16) (value uint16) {
 func StrToInt(input string) uint16 {
 	num, err := strconv.Atoi(input)
 	if err != nil {
-		panic("could not get input number")
+		return outputs[input]
 	}
 	return uint16(num)
 }
