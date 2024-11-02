207f1f7754e6bb7fff40c232f774b31e4013a4e8
IDOMATH <btdothemath@gmail.com>
2024-11-01 21:40:40 -0500

    Continuing part2

--- 2023/Day1/01.go
+++ 2023/Day1/01.go
@@ -12,11 +12,47 @@ func main() {
 	runLogic("./input01.txt")
 }
 
-func transcribeNumber(str string) int {
+func transcribeFirstNumber(str string) int {
 	var zeroIdx, oneIdx, twoIdx, threeIdx, fourIdx, fiveIdx, sixIdx, sevenIdx, eightIdx, nineIdx, curIdx int
 	curIdx = strings.Index(str, "zero")
 	if curIdx != -1 {
 		zeroIdx = curIdx
+	}
+	curIdx = strings.Index(str, "one")
+	if curIdx != -1 {
+		oneIdx = curIdx
+	}
+	curIdx = strings.Index(str, "two")
+	if curIdx != -1 {
+		twoIdx = curIdx
+	}
+	curIdx = strings.Index(str, "three")
+	if curIdx != -1 {
+		threeIdx = curIdx
+	}
+	curIdx = strings.Index(str, "four")
+	if curIdx != -1 {
+		fourIdx = curIdx
+	}
+	curIdx = strings.Index(str, "five")
+	if curIdx != -1 {
+		fiveIdx = curIdx
+	}
+	curIdx = strings.Index(str, "six")
+	if curIdx != -1 {
+		sixIdx = curIdx
+	}
+	curIdx = strings.Index(str, "seven")
+	if curIdx != -1 {
+		sevenIdx = curIdx
+	}
+	curIdx = strings.Index(str, "eight")
+	if curIdx != -1 {
+		eightIdx = curIdx
+	}
+	curIdx = strings.Index(str, "nine")
+	if curIdx != -1 {
+		nineIdx = curIdx
 	}
 }
 
