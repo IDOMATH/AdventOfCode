48173101bbfbd6024a2a21f098652d8d44711a66
IDOMATH <btdothemath@gmail.com>
2024-11-11 20:40:22 -0600

    Continuing setup

--- 2023/Day2/main.go
+++ 2023/Day2/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/idomath/AdventOfCode/util"
 )
@@ -15,7 +17,31 @@ func logic(filepath string) {
 	if err != nil {
 		panic("error getting file")
 	}
-	for _, line := util.GetLines(file){
-		
+	lines := util.GetLines(file)
+	var sum = 0
+	var redPieces = 12
+	var greenPieces = 13
+	var bluePieces = 14
+	for _, line := range lines {
+		groupAndGames := strings.Split(line, ":")
+		blankAndGroup := strings.Split(groupAndGames[0], " ")
+		groupNumber := blankAndGroup[1]
+
+		games := strings.Split(groupAndGames[1], ";")
+		for _, game := range games {
+			pieceMap := make(map[string]int)
+			draws := strings.Split(game, ",")
+			for _, draw := range draws {
+				colorVal := strings.Split(draw, " ")
+				for i := 1; i < len(colorVal); i += 2 {
+					numPieces, err := strconv.Atoi(colorVal[i+1])
+					if err != nil {
+						panic("error getting number of pieces" + err.Error())
+					}
+					pieceMap[colorVal[i]] = numPieces
+				}
+			}
+		}
+		game1 := strings.TrimSpace(games[0])
 	}
 }
